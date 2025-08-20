f15a93c38bcc0c9201a50ccc0779131f7f931637
kyla-rc <kyla@refuelcreative.com.au>
2025-08-20 10:07:26 +0930

    Updated updates view to change depending on whether adding or updating, added functionality for both

--- index.js
+++ index.js
@@ -12,10 +12,14 @@ app.use(express.json());
 // * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
 const PRIVATE_APP_ACCESS = process.env.ACCESS_TOKEN;
 
+const petsAPIName = 'p_pets';
+
+let petData = [];
+
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
 app.get('/', async (req, res) => {
-    const pets = 'https://api.hubapi.com/crm/v3/objects/p_pets?properties=name,pet_type,date_of_birth';
+    const pets = `https://api.hubapi.com/crm/v3/objects/${petsAPIName}?properties=name,pet_type,date_of_birth`;
     
     const headers = {
         Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
@@ -23,8 +27,8 @@ app.get('/', async (req, res) => {
     }
     try {
         const resp = await axios.get(pets, { headers });
-        const data = resp.data.results;
-        res.render('homepage', { title: 'Pets Table | Integrating With HubSpot I Practicum', data });      
+        petData = resp.data.results;
+        res.render('homepage', { title: 'Pets Table | Integrating With HubSpot I Practicum', petData });      
     } catch (error) {
         console.error(error);
     }
@@ -33,12 +37,54 @@ app.get('/', async (req, res) => {
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
 app.get('/update-cobj', (req, res) => {
-    res.render('updates', { title: 'Update Custom Object Form | Integrating with HubSpot I Practicum' });
+    let existingPet = null;
+
+    // If pet query exists, we are updating and need to get the existing pet data
+    if (req.query.pet) {
+        existingPet = petData.find(pet => pet.id === req.query.pet);
+
+        console.log(existingPet);
+    }
+
+    res.render('updates', { title: 'Update Custom Object Form | Integrating with HubSpot I Practicum', existingPet });
 });
 
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
 
 // * Code for Route 3 goes here
+
+app.post('/update-cobj', async (req, res) => {
+    const update = {
+        properties: {
+            "name": req.body.name,
+            "pet_type": req.body.animal,
+            "date_of_birth": req.body.dob
+        }
+    }
+
+    const petId = req.query.pet;
+    const isUpdating = petId;
+    const createPet = `https://api.hubapi.com/crm/v3/objects/${petsAPIName}`;
+    const updatePet = `${createPet}/${petId}`;
+
+    const headers = {
+        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        'Content-Type': 'application/json'
+    };
+
+    try {
+        if (isUpdating) {
+            await axios.patch(updatePet, update, { headers } );
+        }
+        else {
+            await axios.post(createPet, update, { headers });
+        }
+        
+        res.redirect(req.get('Referrer') || '/');
+    } catch(err) {
+        console.error(err);
+    }
+})
 
 /** 
 * * This is sample code to give you a reference for how you should structure your calls. 
